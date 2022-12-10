0c8f2a7dc6625b19cf0a3e6b017970ca93bae970
Mike O'Connor <mike@pineview.net>
2022-12-11 09:49:26 +1030

    Updated sub modules and my flashing script

--- auto_rebuild.sh
+++ auto_rebuild.sh
@@ -1,16 +1,24 @@
 cd ~/klipper
 systemctl stop klipper.service
+
 # Octopus Flash
 make clean KCONFIG_CONFIG=config.Octopus
 #make menuconfig KCONFIG_CONFIG=config.Octopus
 make KCONFIG_CONFIG=config.Octopus
 #~/plugins/CanBoot/scripts/flash_can.py -d /dev/serial/by-id/usb-CanBoot_stm32f446xx_4A002E000250534E4E313120-if00
 ~/plugins/CanBoot/scripts/flash_can.py -i can0 -u 99196bb712d5 -f ~/klipper/out/klipper.bin
-sudo systemctl start klipper.service
+
+# ShyFly Flash
+make clean KCONFIG_CONFIG=config.ShyFly
+make menuconfig KCONFIG_CONFIG=config.ShyFly
+make KCONFIG_CONFIG=config.ShyFly
+#~/plugins/CanBoot/scripts/flash_can.py -d /dev/serial/by-id/usb-CanBoot_stm32f446xx_4A002E000250534E4E313120-if00
+~/plugins/CanBoot/scripts/flash_can.py -i can0 -u 517b54947a42 -f ~/klipper/out/klipper.bin
 
 # Linux MCU
 make clean KCONFIG_CONFIG=config.Linux
 #make menuconfig KCONFIG_CONFIG=config.Linux
 make KCONFIG_CONFIG=config.Linux
 sudo ./scripts/flash-linux.sh
+
 systemctl start klipper.service
